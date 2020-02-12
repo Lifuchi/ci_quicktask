11fa88d0c98997121cddf23f30804e2adbfd26a2
Rifka Annisa <Geminigirl124@gmail.com>
2020-02-12 22:59:40 +0700

    fton

--- application/views/_partials/js.php
+++ application/views/_partials/js.php
@@ -37,6 +37,8 @@
 
 
 
+
+
 		<!-- <script src="assets/vendor/jquery-ui/js/jquery-ui-1.10.4.custom.js"></script> -->
 		<!-- <script src="assets/vendor/jquery-ui-touch-punch/jquery.ui.touch-punch.js"></script> -->
 		<!-- <script src="assets/vendor/jquery-appear/jquery.appear.js"></script> -->
@@ -53,6 +55,18 @@
 		<!-- <script src="assets/vendor/gauge/gauge.js"></script> -->
 		<!-- <script src="assets/vendor/snap-svg/snap.svg.js"></script> -->
 		<!-- <script src="assets/vendor/liquid-meter/liquid.meter.js"></script> -->
+
+
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/jquery.vmap.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/data/jquery.vmap.sampledata.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/maps/jquery.vmap.world.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/maps/continents/jquery.vmap.africa.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/maps/continents/jquery.vmap.asia.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/maps/continents/jquery.vmap.australia.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/maps/continents/jquery.vmap.europe.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/maps/continents/jquery.vmap.north-america.js"></script>
+    <script src="<?php echo base_url(); ?>assets/vendor/jqvmap/maps/continents/jquery.vmap.south-america.js"></script>
+
 
 		<!-- <script src="assets/vendor/jqvmap/jquery.vmap.js"></script>
 		<script src="assets/vendor/jqvmap/data/jquery.vmap.sampledata.js"></script>
